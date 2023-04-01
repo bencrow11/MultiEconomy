05cd4f47fa38549d72867546105df0bfb704f242
bencrow11 <bencrow11@gmail.com>
2023-04-01 15:47:53 +0100

    First commit

---
+++ src/main/java/com/bencrow11/multieconomy/Multieconomy.java
@@ -0,0 +1,9 @@
+package com.bencrow11.multieconomy;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Multieconomy implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/bencrow11/multieconomy/client/MultieconomyClient.java
@@ -0,0 +1,13 @@
+package com.bencrow11.multieconomy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MultieconomyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}

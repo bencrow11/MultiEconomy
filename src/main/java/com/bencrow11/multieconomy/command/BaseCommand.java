8babd8208fb1250135a9205825fb2201207c284c
bencrow11 <bencrow11@gmail.com>
2023-04-16 21:17:19 +0100

    Updated LICENSE

--- src/main/java/com/bencrow11/multieconomy/command/BaseCommand.java
+++ src/main/java/com/bencrow11/multieconomy/command/BaseCommand.java
@@ -1,8 +1,18 @@
+/**
+ * This file is part of MultiEconomy.
+ *
+ * MultiEconomy is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * MultiEconomy is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.bencrow11.multieconomy.command;
 
 import com.bencrow11.multieconomy.Multieconomy;
 import com.bencrow11.multieconomy.command.commands.*;
-import com.bencrow11.multieconomy.util.Utils;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.LiteralCommandNode;
